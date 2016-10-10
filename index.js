46fcbf8d9bd1263c6e3503aabfcc8428cc721249
Dan MacTough <danmactough@gmail.com>
2016-10-10 01:00:30 -0400

    Rewrite to work with old node versions

--- index.js
+++ index.js
@@ -1,57 +1,87 @@
-'use strict';
-const extname = require('path').extname;
-const $ = require('child_process').spawnSync;
+var extname = require('path').extname;
+var exec = require('child_process').exec;
+var spawn = require('child_process').spawn;
 
-module.exports = function (options) {
-  // Check if we have previous commits or this is the initial commit
-  const hasPreviousCommits = $('git', ['rev-parse', '--verify', 'HEAD'], { encoding: 'utf8' }).status === 0;
-  // If we have previous commits, diff against HEAD, otherwise an empty tree object
-  const against = hasPreviousCommits ? 'HEAD' : '4b825dc642cb6eb9a060e54bf8d69288fbee4904';
+module.exports = function (options, done) {
+  findPreviousCommits(function (err, hasPreviousCommits) {
+    if (err) return done(err);
+    // If we have previous commits, diff against HEAD, otherwise an empty tree object
+    var against = hasPreviousCommits ? 'HEAD' : '4b825dc642cb6eb9a060e54bf8d69288fbee4904';
 
-  let diffIndexOptions = [
-    'diff-index',
-    '--diff-filter=AM',
-    '--name-only',
-    against
-  ];
-
-  if (options.c) {
-    diffIndexOptions.push('--cached');
-  }
-
-  // List all javascripty files to be committed
-  const diffIndex = $('git', diffIndexOptions, { encoding: 'utf8' });
-  let files;
-
-  if (diffIndex.status === 0) {
-    let extensions = [
-      'js'
+    var diffIndexOptions = [
+      'diff-index',
+      '--diff-filter=AM',
+      '--name-only',
+      against
     ];
-    if (options.json) {
-      extensions.push('json');
-    }
-    if (options.jsx) {
-      extensions.push('jsx');
-    }
-    const jsExtensionRegex = new RegExp(`^\.(?:${extensions.join('|')})$`, 'i');
-    files = diffIndex.stdout.split('\n').filter(file => {
-      const extension = extname(file);
-      return jsExtensionRegex.test(extension);
-    });
-  }
-  else {
-    return onError(diffIndex.stderr, diffIndex.status);
-  }
 
-  // If we found any files to check, run eslint on them
-  // otherwise, just exit -- nothing to do
-  if (files.length > 0) {
-    const eslintResults = $('eslint', files, { encoding: 'utf8', stdio: 'inherit' });
-    process.exit(eslintResults.status);
-  }
+    if (options.c) {
+      diffIndexOptions.push('--cached');
+    }
+
+    listStagedFiles(diffIndexOptions, function (err, stagedFiles) {
+      if (err) return done(err);
+
+      var files;
+      var extensions = [
+        'js'
+      ];
+
+      if (options.json) {
+        extensions.push('json');
+      }
+      if (options.jsx) {
+        extensions.push('jsx');
+      }
+
+      var jsExtensionRegex = new RegExp('^\.(?:' + extensions.join('|') + ')$', 'i');
+
+      files = stagedFiles.filter(function (file) {
+        var extension = extname(file);
+        return jsExtensionRegex.test(extension);
+      });
+
+      // If we found any files to check, run eslint on them
+      // otherwise, just exit -- nothing to do
+      if (files.length > 0) {
+        lintFiles(files);
+      }
+      else {
+        done();
+      }
+    });
+  });
 };
 
-function onError (msg, code) {
-  console.error(msg);
-  process.exit(code);
+// Check if we have previous commits or this is the initial commit
+function findPreviousCommits (cb) {
+  var child = exec('git rev-parse --verify HEAD', { encoding: 'utf8' }, function (err, stdout, stderr) { // eslint-disable-line no-unused-vars
+    if (err) return cb(buildError(err, child.exitCode));
+    else return cb(null, child.exitCode === 0);
+  });
+}
+
+// List all javascripty files to be committed
+function listStagedFiles (diffIndexOptions, cb) {
+  var child = exec('git ' + diffIndexOptions.join(' '), { encoding: 'utf8' }, function (err, stdout, stderr) {
+    if (err) return cb(buildError(err, child.exitCode));
+    else if (child.exitCode !== 0) return cb(buildError(stderr, child.exitCode));
+    else {
+      var stagedFiles = stdout.split('\n');
+      return cb(null, stagedFiles);
+    }
+  });
+}
+
+function lintFiles (files) {
+  var child = spawn('eslint', files, { encoding: 'utf8', stdio: 'inherit' });
+  child.on('close', function (code) {
+    process.exit(code);
+  });
+}
+
+function buildError (msg, code) {
+  var error = new Error(msg);
+  error.code = code;
+  return error;
 }
